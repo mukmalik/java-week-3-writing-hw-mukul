de923c6a809e107b0d544c03a7a92a9f7631b1f0
Mukmalik <om.mukul.malik@gmail.com>
2024-05-09 21:23:40 +0100

    java-week-3-writing-hw-mukul

---
+++ src/Programme_1_OddEvenTernaryOperator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Programme_1_OddEvenTernaryOperator {
+
+    public static void main(String[] args)
+    {
+        //Scanner declaration for reading input form console
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+        int number = scanner.nextInt();
+        isItOddOrEvenNumber(number);
+        // closing the scanner object
+        scanner.close();
+    }
+
+    // Checking the number is even or odd
+    public static void isItOddOrEvenNumber(int number)
+    {
+        //ternary operator is used
+        String evenOrOdd = (number %2 == 0) ?"Even" : "Odd";
+        System.out.println("The " + number + "is "+evenOrOdd + "number");
+    }
+}
